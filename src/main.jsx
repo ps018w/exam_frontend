8a9a53e95c3538561955c2fdbad55381860c951e
Balram Mandal <balrammandal@Balrams-MacBook-Air.local>
2024-05-06 09:08:29 +0530

    route attribute changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx';
 import './index.css';
 import { store } from './Store/store.js';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
